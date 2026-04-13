80ff918ee5f8223d0b1b54501b6b2fe7b6fe838a
Мальков Олег <karielka@yandex.ru>
2026-04-13 16:29:59 +0300

    fix linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    ignores: ["node_modules/**", "**/*.precompiled.js"],
+    ignores: ["node_modules/**", "**/*.precompiled.js", "dist/**"],
   },
   {
     files: ["server/**/*.js"],
